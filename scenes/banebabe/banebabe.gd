6236858c1ed20023460e2e40f97b6aceb0918e5e
Pablo Pena <pablo_penna7@hotmail.com>
2025-01-05 13:06:28 +0000

    implement acceleration

--- scenes/banebabe/banebabe.gd
+++ scenes/banebabe/banebabe.gd
@@ -1,9 +1,16 @@
 class_name Banebabe extends CharacterBody2D
 
-@export_range(50, 300)
-var move_speed := 100
-@export_range(1, 5)
-var steering_speed := 2.0
+var min_move_speed := 100.0
+var max_move_speed := 600.0
+var acceleration_speed := 2.0
+var deceleration_speed := 2.0
+@onready
+var move_speed := min_move_speed
+
+var min_speed_steering_speed := 6.0
+var max_speed_steering_speed := 1.0
+@onready
+var steering_speed := min_speed_steering_speed
 
 signal collided
 
@@ -23,6 +30,18 @@ func _process_movement(delta:float) -> void:
 		collided.emit()
 	
 	move_and_collide(velocity * delta, false)
+	
+func _process_speed(delta: float) -> void:
+	if Input.is_action_pressed("player_accelerate"):
+		move_speed = lerpf(move_speed, max_move_speed, acceleration_speed * delta)
+		steering_speed = lerpf(steering_speed, max_speed_steering_speed, acceleration_speed * delta)
+	else: 
+		move_speed = lerpf(move_speed, min_move_speed, deceleration_speed * delta)
+		steering_speed = lerp(steering_speed, min_speed_steering_speed, deceleration_speed * delta)
+		
+	move_speed = clampf(move_speed, min_move_speed, max_move_speed)
+	steering_speed = clampf(steering_speed, max_speed_steering_speed, min_speed_steering_speed)
 
 func _physics_process(delta: float) -> void:
+	_process_speed(delta)
 	_process_movement(delta)

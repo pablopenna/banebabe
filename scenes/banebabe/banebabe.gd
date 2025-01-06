4df066ff1cfc316974dd237e82956ef7bd87887c
Pablo Pena <pablo_penna7@hotmail.com>
2025-01-06 08:39:33 +0000

    add drifting mechanic

--- scenes/banebabe/banebabe.gd
+++ scenes/banebabe/banebabe.gd
@@ -4,6 +4,11 @@ var min_move_speed := 100.0
 var max_move_speed := 600.0
 var acceleration_speed := 2.0
 var deceleration_speed := 2.0
+var drifting_lower_threshold := 150
+var drifting_upper_threshold := 350
+var drift_duration := 1.0
+@onready
+var is_drifting := false
 @onready
 var move_speed := min_move_speed
 
@@ -13,24 +18,19 @@ var max_speed_steering_speed := 1.0
 var steering_speed := min_speed_steering_speed
 
 signal collided
+signal drifted
 
-func get_target_direction() -> Vector2:
-	return global_position.direction_to(get_global_mouse_position())
+func _physics_process(delta: float) -> void:
+	_process_drifting()
+	_process_speed(delta)
+	_process_movement(delta)
 
-func _process_movement(delta:float) -> void:
-	var current_direction := velocity.normalized()
-	var target_direction := get_target_direction()
-	var direction := current_direction.lerp(target_direction, steering_speed * delta)
-	
-	velocity = direction * move_speed
-	var collision := move_and_collide(velocity * delta, true)
-	
-	if collision != null:
-		velocity = velocity.bounce(collision.get_normal()) * 2
-		collided.emit()
-	
-	move_and_collide(velocity * delta, false)
-	
+func _process_drifting() -> void:
+	if not is_drifting and _is_drift_input_performed_in_this_frame():
+		is_drifting = true
+		drifted.emit()
+		get_tree().create_timer(drift_duration).timeout.connect(func(): is_drifting = false)
+
 func _process_speed(delta: float) -> void:
 	if Input.is_action_pressed("player_accelerate"):
 		move_speed = lerpf(move_speed, max_move_speed, acceleration_speed * delta)
@@ -42,6 +42,31 @@ func _process_speed(delta: float) -> void:
 	move_speed = clampf(move_speed, min_move_speed, max_move_speed)
 	steering_speed = clampf(steering_speed, max_speed_steering_speed, min_speed_steering_speed)
 
-func _physics_process(delta: float) -> void:
-	_process_speed(delta)
-	_process_movement(delta)
+func _process_movement(delta:float) -> void:
+	velocity = _get_direction(delta) * move_speed
+	var collision := move_and_collide(velocity * delta, true)
+	
+	if collision != null:
+		velocity = velocity.bounce(collision.get_normal()) * 2
+		collided.emit()
+	
+	move_and_collide(velocity * delta, false)
+
+func _get_direction(delta: float) -> Vector2:
+	var current_direction := velocity.normalized()
+	var target_direction := get_target_direction()
+	var direction: Vector2
+	if is_drifting:
+		direction = target_direction
+	else:
+		direction = current_direction.lerp(target_direction, steering_speed * delta)
+	return direction
+
+func get_target_direction() -> Vector2:
+	return global_position.direction_to(get_global_mouse_position())
+
+func _is_in_drifting_range() -> bool:
+	return move_speed >= drifting_lower_threshold and move_speed <= drifting_upper_threshold
+	
+func _is_drift_input_performed_in_this_frame() -> bool:
+	return Input.is_action_just_released("player_accelerate") and _is_in_drifting_range()
